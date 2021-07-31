c661ed6e2cba35488cbc56016d1de1cf0db2d552
Anastasia Rapp <zmraxton@gmail.com>
2021-07-31 16:14:42 -0600

    wall jump and goal prototype

--- SnowWarning-GameJam/Assets/Ana/Scripts/Player.cs
+++ SnowWarning-GameJam/Assets/Ana/Scripts/Player.cs
@@ -15,6 +15,13 @@ public class Player : MonoBehaviour
     Vector3 velocity;
     Vector2 mousePos { get => Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()); }
 
+    Rigidbody2D rb;
+
+    public bool canJump;
+
+    [SerializeField] float gracePeriod;
+    float graceTimer = 0;
+
     PlayerControls playerControls;
     float dt { get => Time.deltaTime; }
 
@@ -27,15 +34,11 @@ public class Player : MonoBehaviour
     private void Start()
     {
         GetComponent<PlayerInput>().onActionTriggered += HandleAction;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        velocity.x = input.x * speed;
-        transform.Translate(velocity * dt);
-
-        velocity = Vector2.zero;
-
         if(fireTimer < fireRate)
         {
             fireTimer += dt;
@@ -45,6 +48,36 @@ public class Player : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        if (rb.IsTouchingLayers(LayerMask.GetMask("Terrain", "WallRight", "WallLeft")))
+        {
+            canJump = true;
+        }
+        else if(graceTimer < gracePeriod)
+        {
+            graceTimer += dt;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        if (rb.IsTouchingLayers(LayerMask.GetMask("WallRight", "WallLeft")))
+        {
+            rb.gravityScale = 0.4f;
+        }
+        else
+        {
+            rb.gravityScale = 2;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        velocity.x = input.x * speed;
+        transform.Translate(velocity * dt);
+
+        velocity = Vector2.zero;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -54,9 +87,20 @@ public class Player : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.action.triggered)
+        if (context.action.triggered && canJump)
         {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+            if(rb.IsTouchingLayers(LayerMask.GetMask("WallRight")))
+            {
+                rb.AddForce(new Vector2 (0.75f, 0.75f) * jumpForce);
+            }
+            else if (rb.IsTouchingLayers(LayerMask.GetMask("WallLeft")))
+            {
+                rb.AddForce(new Vector2(-0.75f, 0.75f) * jumpForce);
+            }
+            else
+            {
+                rb.AddForce(Vector2.up * jumpForce);
+            }
         }
     }
 
@@ -68,6 +112,11 @@ public class Player : MonoBehaviour
             curLight.GetComponent<ShootLight>().destination = mousePos;
             fireTimer = 0;
         }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        graceTimer = 0;
     }
 
     private void HandleAction(InputAction.CallbackContext context)
